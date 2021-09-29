99a93fad57eada9bb06a1358c456ae28da20d4e7
Joanna <m.joanna.foss@gmail.com>
2021-09-29 11:46:08 -0500

    updated: completed konami

--- js/jquery.js
+++ js/jquery.js
@@ -4,11 +4,15 @@ console.log("test");
 
 var userValue = '';
 
-$('body').keydown(function(event) {
+
+
+$('body').keyup(function(event) {
 	console.log(event.key);
 	userValue += event.key;
-	if(userValue === "test"){
-		alert("Booyah!");
+	if(userValue === "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter"){
+		$('h1').html("S U N L I G H T").css('color', 'white');
+		$('body').css('background-image', 'url(../img/msg-wallpaper.png)').css('background-size', 'cover');
+		$('#playMe').html('<audio autoplay><source src="../audio/Mgs4_Snake_sunlight!!.mp3" type="audio/mpeg"></audio>');
 	}
 });
 
