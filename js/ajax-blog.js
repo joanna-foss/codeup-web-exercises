3ac32ea6f93eb85b587699b1e9d363589bc74cb6
Joanna <m.joanna.foss@gmail.com>
2021-10-03 13:51:03 -0500

    completed: ajax blog

---
+++ js/ajax-blog.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$.ajax("data/blog.json").done(function(data){
+	console.log(data);
+	for(let i = 0; i < data.length; i++){
+		let insertHTML =
+			'<h2 class="mt-3 text-center">' + data[i].title.toLowerCase() + '</h2>' +
+			'<div class="d-flex text-black-50 justify-content-between">' +
+			'<div>' + "date: " + data[i].date.toLowerCase() + '</div>' +
+			'<div>' + "categories: " + data[i].categories + '</div>' +
+			'</div>' + //end row
+			'<div>' + data[i].content + '</div>'
+		$(".posts").append(insertHTML);
+	}
+});
