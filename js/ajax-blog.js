96dafded86ced77b529b1bb767330f8eaa71d301
Joanna <m.joanna.foss@gmail.com>
2021-10-05 09:38:44 -0500

    updated: edits from shanshan review

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -7,7 +7,7 @@ $.ajax("data/blog.json").done(function(data){
 			'<h2 class="mt-3 text-center">' + data[i].title.toLowerCase() + '</h2>' +
 			'<div class="d-flex text-black-50 justify-content-between">' +
 			'<div>' + "date: " + data[i].date.toLowerCase() + '</div>' +
-			'<div>' + "categories: " + data[i].categories + '</div>' +
+			'<div>' + "categories: " + data[i].categories.join(' || ') + '</div>' +
 			'</div>' + //end row
 			'<div>' + data[i].content + '</div>'
 		$(".posts").append(insertHTML);
