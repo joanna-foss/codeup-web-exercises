06834ef2645f5bc29bc61857ec626f134eb3425f
Joanna <m.joanna.foss@gmail.com>
2021-10-07 11:57:21 -0500

    update: created default city weather page

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// $(document).ready(function(elem){
+//     console.log("ready");
+// });
+
+let defaultCity = {};
+let cityDeets = '';
+let dayTime = '';
+
 $.get("https://api.openweathermap.org/data/2.5/onecall", {
     APPID: openweathermapAPIKey,
     // q:     "San Antonio, US", //q, id, and lat&long are interchangeable
@@ -9,4 +17,29 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
     units: "imperial" //without this, standard temperature type is Kelvin; use "imperial" for F and "metric" for C
 }).done(function(data){ //do this first to see the shape of the data
     console.log(data); //and so you can determine what you'll need to do to use it
+
+    defaultCity.lat = data.lat;
+    defaultCity.lng = data.lon;
+    defaultCity.temp = data.current.temp;
+    defaultCity.date = convertToDayTime(data.current.dt);
+
+    reverseGeocode(defaultCity, mapboxAPIKey).then(function(data){
+        cityDeets = data;
+        console.log(cityDeets);
+        $('#city').html('<strong>Your City</strong>:<br>' + cityDeets);
+        $('#time').html('<strong>Today\'s Date</strong>:<br>' + dayTime);
+        $('#temp').html('<strong>Today\'s Temperature</strong>: <br>' + defaultCity.temp + ' °F');
+    });
+
 });
+
+
+function convertToDayTime(dt){ //data.current.dt
+    let date = new Date(dt*1000);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = date.getFullYear();
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+
+    dayTime = month + ' ' + day + ', ' + year;
+}
