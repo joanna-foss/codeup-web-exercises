9c807246fcea830f919fd37c285dd328098ca449
Joanna <m.joanna.foss@gmail.com>
2021-10-08 16:00:24 -0500

    completed: minor edits

--- js/weather_map.js
+++ js/weather_map.js
@@ -27,15 +27,20 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
 	defaultCity.lng = data.lon;
 	defaultCity.temp = data.current.temp;
 	defaultCity.date = convertToDayTime(data.current.dt);
+	// defaultCity.high = data.daily.temp.max;
+	// defaultCity.low = data.daily.temp.min;
+
+	// console.log(data.daily.temp.max);
 
 	reverseGeocode(defaultCity, mapboxAPIKey).then(function(data){
 		$('#city').html('<strong>Your Location</strong>:<br>' + data);
 		$('#date').html('<strong>Today\'s Date</strong>:<br>' + defaultCity.date);
 		$('#temp').html('<strong>Current Temperature</strong>: <br>' + parseInt(defaultCity.temp) + ' °F');
+		// $('#highs-lows').html('<strong>Today\'s High / Low</strong>: <br>' + parseInt(defaultCity.high) + ' °F / ' + parseInt(defaultCity.low) + ' °F');
 		$('.city-coords').html(defaultCity.lat + ', ' + defaultCity.lng);
 	});
 
-	for (let i = 1; i <= 5; i++) {
+	for (let i = 1; i <= 5; i++){
 		let days = ['.day1', '.day2', '.day3', '.day4', '.day5'];
 		let html =
 			'<h6 class="card-header">' + convertToDayTime(data.daily[i].dt) + '</h6>' +
@@ -59,8 +64,9 @@ function convertToDayTime(dt){
 	return dayTime;
 }
 
-let marker = new mapboxgl.Marker({draggable: true})
+let marker = new mapboxgl.Marker({draggable: true, color: 'red'});
 
+//CLICK ON MAP
 map.on('click', function(e){
 	console.log(e);
 
@@ -81,10 +87,10 @@ map.on('click', function(e){
 			lat: newCoords.lat,
 			lon: newCoords.lng,
 			units: "imperial"
-		}).done(function (data) {
+		}).done(function(data){
 			console.log(data);
 			//POPULATE THE 5 DAY FORECAST CARDS HERE
-			for (let i = 1; i <= 5; i++) {
+			for (let i = 1; i <= 5; i++){
 				let days = ['.day1', '.day2', '.day3', '.day4', '.day5'];
 				let html =
 					'<h6 class="card-header">' + convertToDayTime(data.daily[i].dt) + '</h6>' +
@@ -102,40 +108,52 @@ map.on('click', function(e){
 		});
 	})
 
-	function onDragEnd(){
+	function onDragEnd() {
 		const coordsOnDrag = marker.getLngLat();
 		console.log(coordsOnDrag);
 
+		let newCoords = {
+			lng: coordsOnDrag.lng,
+			lat: coordsOnDrag.lat
+		};
+
+		//PULL CITY WEATHER INFO FROM OPEN WEATHER MAP
+		reverseGeocode(newCoords, mapboxAPIKey).then(function(data){
+			console.log(data);
+			$('#city').html('<strong>Chosen Location</strong>:<br>' + data);
+
+			$.get("https://api.openweathermap.org/data/2.5/onecall", {
+				APPID: openweathermapAPIKey,
+				lat: newCoords.lat,
+				lon: newCoords.lng,
+				units: "imperial"
+			}).done(function (data) {
+				console.log(data);
+				//POPULATE THE 5 DAY FORECAST CARDS HERE
+				for (let i = 1; i <= 5; i++){
+					let days = ['.day1', '.day2', '.day3', '.day4', '.day5'];
+					let html =
+						'<h6 class="card-header">' + convertToDayTime(data.daily[i].dt) + '</h6>' +
+						'<div class=\"card-text p-3\">' + '<strong>High / Low:</strong><br>' + data.daily[i].temp.max + '°F / ' + data.daily[i].temp.min + '°F <br>' + '<img src="http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png\">' + '</div>' +
+						'<div class=\"card-text p-1\"><strong>Description:</strong><br>' + data.daily[i].weather[0].description + '</div>' +
+						'<div class=\"card-text p-1\">' + '<strong>Humidity:</strong><br>' + data.daily[i].humidity + ' %</div>' +
+						'<div class=\"card-text p-1\">' + '<strong>Wind Speed:</strong><br>' + data.daily[i].wind_speed + '</div>' +
+						'<div class=\"card-text p-1\">' + '<strong>Pressure:</strong><br>' + data.daily[i].pressure + '</div>'
+
+					$(days[i - 1]).html(html);
+
+					$('#temp').html('<strong>Current Temperature</strong>: <br>' + parseInt(data.current.temp) + ' °F');
+					$('.city-coords').html(newCoords.lng + ', ' + newCoords.lat);
+				}
+			});
+		});
 	}
 
 	marker.on('dragend', onDragEnd);
 });
 
-// function onDrag(){
-//
-// }
-
-// var marker = new mapboxgl.Marker()
-// .setLngLat([-98.4916, 29.4260])
-// .addTo(map);
-//
-// const marker = new mapboxgl.Marker({
-// draggable: true
-// })
-// .setLngLat([0, 0])
-// .addTo(map);
-//
-// function onDragEnd() {
-// const lngLat = marker.getLngLat();
-// coordinates.style.display = 'block';
-// coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
-// }
-//
-// marker.on('dragend', onDragEnd);
-
-
-//ON CHANGE LOCATION BUTTON CLICK
-document.getElementById('new-city-please').addEventListener('click', function (e) {
+//ON BUTTON CLICK
+document.getElementById('new-city-please').addEventListener('click', function(e){
 	//4 VARIABLES TO SEARCH
 	let streetInput = document.querySelector("#new-city-form input:nth-child(2)").value;
 	let cityInput = document.querySelector("#new-city-form input:nth-child(3)").value;
@@ -143,11 +161,11 @@ document.getElementById('new-city-please').addEventListener('click', function (e
 	let newSearch = streetInput + " " + cityInput + " " + stateInput;
 	//END VARIABLES
 
-	//PULL CITY DATA FROM MAPBOX
-	geocode(newSearch, mapboxAPIKey).then(function (data) {
-		console.log(data);
+	//MAPBOX
+	geocode(newSearch, mapboxAPIKey).then(function(data){
+		// console.log(data);
 		map.flyTo({center: data, zoom: 15}); //zoom into new city
-		new mapboxgl.Marker().setLngLat(data).addTo(map); //add new marker to map
+		new mapboxgl.Marker({color: 'black'}).setLngLat(data).addTo(map); //add new marker to map
 
 		let newDD = {
 			lng: data[0],
@@ -156,17 +174,15 @@ document.getElementById('new-city-please').addEventListener('click', function (e
 
 		console.log(newDD);
 
-		//PULL CITY WEATHER INFO FROM OPEN WEATHER MAP
+		//WEATHER INFO
 		$.get("https://api.openweathermap.org/data/2.5/onecall", {
 			APPID: openweathermapAPIKey,
-			// q:     "San Antonio, US", //q, id, and lat&long are interchangeable
-			// id: 4726206, //q, id, and lat&long are interchangeable
 			lat: newDD.lat, //q, id, and lat&long are interchangeable
 			lon: newDD.lng, //q, id, and lat&long are interchangeable
 			units: "imperial" //without this, standard temperature type is Kelvin; use "imperial" for F and "metric" for C
-		}).done(function (data) {
+		}).done(function(data){
 			//POPULATE THE 5 DAY FORECAST CARDS HERE
-			for (let i = 1; i <= 5; i++) {
+			for (let i = 1; i <= 5; i++){
 				let days = ['.day1', '.day2', '.day3', '.day4', '.day5'];
 				let html =
 					'<h6 class="card-header">' + convertToDayTime(data.daily[i].dt) + '</h6>' +
@@ -176,25 +192,17 @@ document.getElementById('new-city-please').addEventListener('click', function (e
 					'<div class=\"card-text p-1\">' + '<strong>Wind Speed:</strong><br>' + data.daily[i].wind_speed + '</div>' +
 					'<div class=\"card-text p-1\">' + '<strong>Pressure:</strong><br>' + data.daily[i].pressure + '</div>'
 
+				//UPDATE CARDS + CURRENT TEMP UP TOP + CITY COORDS IN NAV AREA
 				$(days[i - 1]).html(html);
 				$('#temp').html('<strong>Current Temperature</strong>: <br>' + parseInt(data.current.temp) + ' °F');
 				$('.city-coords').html(newDD.lng+', ' +newDD.lat);
 
-
-				reverseGeocode(newDD, mapboxAPIKey).then(function (data) {
+				//UPDATE LOCATION UP TOP
+				reverseGeocode(newDD, mapboxAPIKey).then(function(data){
 					console.log(data);
 					$('#city').html('<strong>Your Location</strong>:<br>' + data);
 				});
 			}
 		});
-
-		//UPDATE THE BIG BOX & COORDINATES IN HEADER INFORMATION HERE
-		// reverseGeocode(newDD, mapboxAPIKey).then(function(data){
-		//     console.log(data);
-		//     $('#city').html('<strong>Your Location</strong>:<br>' + data);
-		//     $('#temp').html('<strong>Current Temperature</strong>: <br>' + parseInt(data.daily.temp) + ' °F');
-		//     $('.city-coords').html(newDD.lat + ', ' + newDD.lng);
-		// });
 	});
-
 });
