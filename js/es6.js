634f1bd1d024a663d5dff4bd569bf010522a5ee2
Joanna <m.joanna.foss@gmail.com>
2021-10-13 09:48:47 -0500

    edited to be more accurate

--- js/es6.js
+++ js/es6.js
@@ -26,11 +26,6 @@ const users = [
     name: 'justin',
     email: 'justin@codeup.com',
     languages: ['html', 'css', 'javascript', 'php']
-  },
-  {
-    name: 'joanna',
-    email: 'joanna@codeup.com',
-    languages: ['html', 'css', 'javascript']
   }
 ];
 
@@ -40,9 +35,9 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-const name = 'your_name_here';
-const email = '';
-const languages = [];
+const name = 'joanna';
+const email = 'joanna@codeup.com';
+const languages = ['html'];
 
 // TODO: rewrite the object literal using object property shorthand
 // users.push({
@@ -52,6 +47,7 @@ const languages = [];
 // });
 
 users.push({name, email, languages});
+// console.log(users);
 
 // TODO: replace `var` with `let` in the following variable declarations
 // let emails = [];
