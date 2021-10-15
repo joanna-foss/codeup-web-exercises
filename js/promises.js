9d32226cd0df57bff4b16d2274132d341ffc7c59
Joanna <m.joanna.foss@gmail.com>
2021-10-15 14:26:40 -0500

    review with tristan

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,6 @@
 // fetch('https://api.github.com/users/joanna-foss/events', {
 // 	method: 'GET',
-// 	headers: {'Authorization': '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'}})
+// 	headers: {'Authorization': ''}})
 // 	.then(response => response.json())
 // 	.then(data => console.log(data[0].created_at))
 // 	.catch(error => console.error(error));
@@ -19,7 +19,7 @@
 function lastCommit(userName){
 	return fetch(`https://api.github.com/users/${userName}/events`, {
 		method: 'GET',
-		headers: {'Authorization': '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'}
+		headers: {'Authorization': ''}
 	})
 	.then(response => response.json())
 	.then(data => {
@@ -33,5 +33,43 @@ console.log(lastCommit('Dezmonelm'));
 //Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 
 function wait(number){
-
+	return new Promise(resolve=>{
+		setTimeout(function(){
+			resolve(`You'll see this message after ${number/1000} seconds!`)
+		}, `${number}`);
+	})
 }
+
+wait(1000).then((message)=>console.log(message));
+wait(3000).then((message)=>console.log(message));
+wait(5000).then((message)=>console.log(message));
+
+//TRISTAN
+
+// const headers = {
+// 	// Authorization: "",
+// 	Accept: "application/vnd.github.v3+json",
+// };
+//
+// function dateOfLastCommit(username){
+// 	return fetch('https://api', {
+// 		headers,
+// 	}).then((eventRes)=>{
+// 		eventRes.json().then((eventJson) => {
+// 			//if the request was unsuccessful, throw error
+// 			if(!eventRes.ok) throw eventJson;
+//
+// 			//look for first event that has type "PushEvent"
+// 			//Returns undefined if one isn't found.
+// 			const lastPush = eventJson.find(event => event.type === "PushEvent");
+//
+// 			// No PushEvent was found... abort!
+// 			if(!lastPush) throw "Could not find last commit!";
+//
+// 			// #region Technically, this would be *more* correct...
+// 			return new Date(lastPush.created_at);
+// 		})
+// 	})
+// }
+
+//And then some but he did not thoroughly explain...
