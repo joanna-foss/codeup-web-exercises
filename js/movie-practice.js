f62b675e0384cac24b0247c73c57d36b573adaa1
Joanna <m.joanna.foss@gmail.com>
2021-10-17 22:42:16 -0500

    working through movie project techniques

--- js/movie-practice.js
+++ js/movie-practice.js
@@ -15,10 +15,35 @@ switch(document.readyState) {
 }
 
 let options = {method: 'GET'} //edit as needed.
-let movies = [];
 
 //THIS GETS YOU THE LIST OF MOVIES FROM THE DATABASE.
-let somedata = fetch('https://wise-citrine-swordfish.glitch.me/movies', options) //<--options here is optional.
-	.then(response=>response.json().then(data=>data));
+fetch('https://wise-citrine-swordfish.glitch.me/movies', options) //<--options here is optional.
+	.then(response=> {
+		if (response.ok) {
+			console.log("success");
+		} else {
+			console.log("not success");
+		}
+		return response.json()
+	})
+	.then(response=>{
+		console.log(response);
+		let html = '';
+		response.forEach(function(elem, index, array){
+			//CBB use newly learned ES6 techniques to pull properties into values perhaps
+			if(typeof elem.title !== 'undefined') {
+				html +=
+					'<div class="card d-flex flex-column align-items-center"><h1>' +
+					elem.title + '</h1><br>' +
+					'<img src="' + elem.poster + '" class="h-50 w-50"><br>' +
+					elem.year + ' // ' + elem.genre + '<br>' +
+					'actors: ' + elem.actors + '<br>' +
+					'directed by: ' + elem.director +
+					'</div>'
+			} else {
+				console.log('unidentified movie log');
+			}
+		})
+		document.getElementById('ogContainer').innerHTML = html})
 
-//I DON"T KNOW WHAT I"M DOING.
+//I DON'T KNOW WHAT I'M DOING.
